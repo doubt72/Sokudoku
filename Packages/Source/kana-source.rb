57c5ff4025a607ed22891402833ed666fc304f6e
doubt72 <douglas@triggs.org>
2013-03-12 19:32:18 -0600

    Added proper kana package

---
+++ Packages/Source/kana-source.rb
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+tags = ["hiragana", "katakana", "both"]
+desc = ["Hiragana", "Katakana", "All Characters"]
+
+chars = [
+         ['あ', ['a'], ['hiragana', 'both']],
+         ['い', ['i'], ['hiragana', 'both']],
+         ['う', ['u'], ['hiragana', 'both']],
+         ['え', ['e'], ['hiragana', 'both']],
+         ['お', ['o'], ['hiragana', 'both']],
+         ['か', ['ka'], ['hiragana', 'both']],
+         ['き', ['ki'], ['hiragana', 'both']],
+         ['く', ['ku'], ['hiragana', 'both']],
+         ['け', ['ku'], ['hiragana', 'both']],
+         ['こ', ['ko'], ['hiragana', 'both']],
+         ['さ', ['sa'], ['hiragana', 'both']],
+         ['し', ['si', 'shi'], ['hiragana', 'both']],
+         ['す', ['su'], ['hiragana', 'both']],
+         ['せ', ['se'], ['hiragana', 'both']],
+         ['そ', ['so'], ['hiragana', 'both']],
+         ['た', ['ta'], ['hiragana', 'both']],
+         ['ち', ['ti', 'chi'], ['hiragana', 'both']],
+         ['つ', ['tu', 'tsu'], ['hiragana', 'both']],
+         ['て', ['te'], ['hiragana', 'both']],
+         ['と', ['to'], ['hiragana', 'both']],
+         ['な', ['na'], ['hiragana', 'both']],
+         ['に', ['ni'], ['hiragana', 'both']],
+         ['ぬ', ['nu'], ['hiragana', 'both']],
+         ['ね', ['ne'], ['hiragana', 'both']],
+         ['の', ['no'], ['hiragana', 'both']],
+         ['は', ['ha'], ['hiragana', 'both']],
+         ['ひ', ['hi'], ['hiragana', 'both']],
+         ['ふ', ['hu', 'fu'], ['hiragana', 'both']],
+         ['へ', ['he', 'e'], ['hiragana', 'both']],
+         ['ほ', ['ho'], ['hiragana', 'both']],
+         ['ま', ['ma'], ['hiragana', 'both']],
+         ['み', ['mi'], ['hiragana', 'both']],
+         ['む', ['mu'], ['hiragana', 'both']],
+         ['め', ['me'], ['hiragana', 'both']],
+         ['も', ['mo'], ['hiragana', 'both']],
+         ['や', ['ya'], ['hiragana', 'both']],
+         ['ゆ', ['yu'], ['hiragana', 'both']],
+         ['よ', ['yo'], ['hiragana', 'both']],
+         ['ら', ['ra'], ['hiragana', 'both']],
+         ['り', ['ri'], ['hiragana', 'both']],
+         ['る', ['ru'], ['hiragana', 'both']],
+         ['れ', ['re'], ['hiragana', 'both']],
+         ['ろ', ['ro'], ['hiragana', 'both']],
+         ['わ', ['wa'], ['hiragana', 'both']],
+         ['ん', ['n'], ['hiragana', 'both']],
+         ['が', ['ga'], ['hiragana', 'both']],
+         ['ぎ', ['gi'], ['hiragana', 'both']],
+         ['ぐ', ['gu'], ['hiragana', 'both']],
+         ['げ', ['ge'], ['hiragana', 'both']],
+         ['ご', ['go'], ['hiragana', 'both']],
+         ['ざ', ['za'], ['hiragana', 'both']],
+         ['じ', ['zi', 'ji'], ['hiragana', 'both']],
+         ['ず', ['zu'], ['hiragana', 'both']],
+         ['ぜ', ['ze'], ['hiragana', 'both']],
+         ['ぞ', ['zo'], ['hiragana', 'both']],
+         ['だ', ['da'], ['hiragana', 'both']],
+         ['ぢ', ['di', 'ji'], ['hiragana', 'both']],
+         ['づ', ['du', 'zu', 'dzu'], ['hiragana', 'both']],
+         ['で', ['de'], ['hiragana', 'both']],
+         ['ど', ['do'], ['hiragana', 'both']],
+         ['ば', ['ba'], ['hiragana', 'both']],
+         ['び', ['bi'], ['hiragana', 'both']],
+         ['ぶ', ['bu'], ['hiragana', 'both']],
+         ['べ', ['be'], ['hiragana', 'both']],
+         ['ぼ', ['bo'], ['hiragana', 'both']],
+         ['ぽ', ['pa'], ['hiragana', 'both']],
+         ['ぴ', ['pi'], ['hiragana', 'both']],
+         ['ぶ', ['pu'], ['hiragana', 'both']],
+         ['ぺ', ['pe'], ['hiragana', 'both']],
+         ['ぽ', ['po'], ['hiragana', 'both']],
+         ['きゃ', ['kya'], ['hiragana', 'both']],
+         ['きゅ', ['kyu'], ['hiragana', 'both']],
+         ['きょ', ['kyo'], ['hiragana', 'both']],
+         ['しゃ', ['sya', 'sha'], ['hiragana', 'both']],
+         ['しゅ', ['syu', 'shu'], ['hiragana', 'both']],
+         ['しょ', ['syo', 'sho'], ['hiragana', 'both']],
+         ['ちゃ', ['tya', 'cha'], ['hiragana', 'both']],
+         ['ちゅ', ['tyu', 'chu'], ['hiragana', 'both']],
+         ['ちょ', ['tyo', 'cho'], ['hiragana', 'both']],
+         ['にゃ', ['nya'], ['hiragana', 'both']],
+         ['にゅ', ['nyu'], ['hiragana', 'both']],
+         ['にょ', ['nyo'], ['hiragana', 'both']],
+         ['ひゃ', ['hya'], ['hiragana', 'both']],
+         ['ひゅ', ['hyu'], ['hiragana', 'both']],
+         ['ひょ', ['hyo'], ['hiragana', 'both']],
+         ['みゃ', ['mya'], ['hiragana', 'both']],
+         ['みゅ', ['myu'], ['hiragana', 'both']],
+         ['みょ', ['myo'], ['hiragana', 'both']],
+         ['りゃ', ['rya'], ['hiragana', 'both']],
+         ['りゅ', ['ryu'], ['hiragana', 'both']],
+         ['りょ', ['ryo'], ['hiragana', 'both']],
+         ['を', ['wo', 'o'], ['hiragana', 'both']],
+         ['ア', ['a'], ['katakana', 'both']],
+         ['イ', ['i'], ['katakana', 'both']],
+         ['ウ', ['u'], ['katakana', 'both']],
+         ['エ', ['e'], ['katakana', 'both']],
+         ['オ', ['o'], ['katakana', 'both']],
+         ['カ', ['ka'], ['katakana', 'both']],
+         ['キ', ['ki'], ['katakana', 'both']],
+         ['ク', ['ku'], ['katakana', 'both']],
+         ['ケ', ['ku'], ['katakana', 'both']],
+         ['コ', ['ko'], ['katakana', 'both']],
+         ['サ', ['sa'], ['katakana', 'both']],
+         ['シ', ['si', 'shi'], ['katakana', 'both']],
+         ['ス', ['su'], ['katakana', 'both']],
+         ['セ', ['se'], ['katakana', 'both']],
+         ['ソ', ['so'], ['katakana', 'both']],
+         ['タ', ['ta'], ['katakana', 'both']],
+         ['チ', ['ti', 'chi'], ['katakana', 'both']],
+         ['ツ', ['tu', 'tsu'], ['katakana', 'both']],
+         ['テ', ['te'], ['katakana', 'both']],
+         ['ト', ['to'], ['katakana', 'both']],
+         ['ナ', ['na'], ['katakana', 'both']],
+         ['ニ', ['ni'], ['katakana', 'both']],
+         ['ヌ', ['nu'], ['katakana', 'both']],
+         ['ネ', ['ne'], ['katakana', 'both']],
+         ['ノ', ['no'], ['katakana', 'both']],
+         ['ハ', ['ha'], ['katakana', 'both']],
+         ['ヒ', ['hi'], ['katakana', 'both']],
+         ['フ', ['hu', 'fu'], ['katakana', 'both']],
+         ['ヘ', ['he', 'e'], ['katakana', 'both']],
+         ['ホ', ['ho'], ['katakana', 'both']],
+         ['マ', ['ma'], ['katakana', 'both']],
+         ['ミ', ['mi'], ['katakana', 'both']],
+         ['ム', ['mu'], ['katakana', 'both']],
+         ['メ', ['me'], ['katakana', 'both']],
+         ['モ', ['mo'], ['katakana', 'both']],
+         ['ヤ', ['ya'], ['katakana', 'both']],
+         ['ユ', ['yu'], ['katakana', 'both']],
+         ['ヨ', ['yo'], ['katakana', 'both']],
+         ['ラ', ['ra'], ['katakana', 'both']],
+         ['リ', ['ri'], ['katakana', 'both']],
+         ['ル', ['ru'], ['katakana', 'both']],
+         ['レ', ['re'], ['katakana', 'both']],
+         ['ロ', ['ro'], ['katakana', 'both']],
+         ['ワ', ['wa'], ['katakana', 'both']],
+         ['ン', ['n'], ['katakana', 'both']],
+         ['ガ', ['ga'], ['katakana', 'both']],
+         ['ギ', ['gi'], ['katakana', 'both']],
+         ['グ', ['gu'], ['katakana', 'both']],
+         ['ゲ', ['ge'], ['katakana', 'both']],
+         ['ゴ', ['go'], ['katakana', 'both']],
+         ['ザ', ['za'], ['katakana', 'both']],
+         ['ジ', ['zi', 'ji'], ['katakana', 'both']],
+         ['ズ', ['zu'], ['katakana', 'both']],
+         ['ゼ', ['ze'], ['katakana', 'both']],
+         ['ゾ', ['zo'], ['katakana', 'both']],
+         ['ダ', ['da'], ['katakana', 'both']],
+         ['ヂ', ['di', 'ji'], ['katakana', 'both']],
+         ['ヅ', ['du', 'zu', 'dzu'], ['katakana', 'both']],
+         ['デ', ['de'], ['katakana', 'both']],
+         ['ド', ['do'], ['katakana', 'both']],
+         ['バ', ['ba'], ['katakana', 'both']],
+         ['ビ', ['bi'], ['katakana', 'both']],
+         ['ブ', ['bu'], ['katakana', 'both']],
+         ['ベ', ['be'], ['katakana', 'both']],
+         ['ボ', ['bo'], ['katakana', 'both']],
+         ['パ', ['pa'], ['katakana', 'both']],
+         ['ピ', ['pi'], ['katakana', 'both']],
+         ['プ', ['pu'], ['katakana', 'both']],
+         ['ペ', ['pe'], ['katakana', 'both']],
+         ['ポ', ['po'], ['katakana', 'both']],
+         ['キャ', ['kya'], ['katakana', 'both']],
+         ['キュ', ['kyu'], ['katakana', 'both']],
+         ['キョ', ['kyo'], ['katakana', 'both']],
+         ['シャ', ['sya', 'sha'], ['katakana', 'both']],
+         ['シュ', ['syu', 'shu'], ['katakana', 'both']],
+         ['ショ', ['syo', 'sho'], ['katakana', 'both']],
+         ['チャ', ['tya', 'cha'], ['katakana', 'both']],
+         ['チュ', ['tyu', 'chu'], ['katakana', 'both']],
+         ['チョ', ['tyo', 'cho'], ['katakana', 'both']],
+         ['ニャ', ['nya'], ['katakana', 'both']],
+         ['ニュ', ['nyu'], ['katakana', 'both']],
+         ['ニョ', ['nyo'], ['katakana', 'both']],
+         ['ヒャ', ['hya'], ['katakana', 'both']],
+         ['ヒュ', ['hyu'], ['katakana', 'both']],
+         ['ヒョ', ['hyo'], ['katakana', 'both']],
+         ['ミャ', ['mya'], ['katakana', 'both']],
+         ['ミュ', ['myu'], ['katakana', 'both']],
+         ['ミョ', ['myo'], ['katakana', 'both']],
+         ['リャ', ['rya'], ['katakana', 'both']],
+         ['リュ', ['ryu'], ['katakana', 'both']],
+         ['リョ', ['ryo'], ['katakana', 'both']],
+         ['フィ', ['fi'], ['katakana', 'both']],
+         ['フェ', ['fe'], ['katakana', 'both']],
+         ['ファ', ['fa'], ['katakana', 'both']],
+         ['フォ', ['fo'], ['katakana', 'both']],
+         ['ウィ', ['wi'], ['katakana', 'both']],
+         ['ウェ', ['we'], ['katakana', 'both']],
+         ['シェ', ['she'], ['katakana', 'both']],
+         ['チェ', ['che'], ['katakana', 'both']],
+         ['デぃ', ['di'], ['katakana', 'both']],
+         ['ヲ', ['wo', 'o'], ['katakana', 'both']]
+        ]
+
+puts "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+puts "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">"
+puts "<plist version=\"1.0\">"
+puts "<dict>"
+puts "  <key>name</key>"
+puts "  <string>Japanese Kana</string>"
+puts "  <key>tags</key>"
+puts "  <array>"
+tags.each do |tag|
+  puts "    <string>#{tag}</string>"
+end
+puts "  </array>"
+puts "  <key>tagDescriptions</key>"
+puts "  <array>"
+desc.each do |d|
+  puts "    <string>#{d}</string>"
+end
+puts "  </array>"
+puts "  <key>characters</key>"
+puts "  <array>"
+chars.each do |char|
+  puts "    <dict>"
+  puts "      <key>literal</key>"
+  puts "      <string>#{char[0]}</string>"
+  puts "      <key>pronunciations</key>"
+  puts "      <array>"
+  char[1].each do |pron|
+    puts "        <string>#{pron}</string>"
+  end
+  puts "      </array>"
+  puts "      <key>tags</key>"
+  puts "      <array>"
+  char[2].each do |tag|
+    puts "        <string>#{tag}</string>"
+  end
+  puts "      </array>"
+  puts "    </dict>"
+end
+puts "  </array>"
+puts "</dict>"
+puts "</plist>"
